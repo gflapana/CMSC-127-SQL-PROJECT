b598992f0a24d87d6b2151034ca7d6630ea0c3c5
Geoffrey Gabriel F. Lapaña <gflapana@up.edu.ph>
2025-05-01 14:42:20 +0800

    feat: add fee table

--- sql/studorg_ddl.sql
+++ sql/studorg_ddl.sql
@@ -3,7 +3,7 @@ create database studorg;
 
 --Create Organization Tabble
 CREATE TABLE organization(
-    organization_id INT(3),
+    organization_id INT(3) NOT NULL,
     organization_name VARCHAR(50) NOT NULL,
     organization_type VARCHAR(20),
     date_established DATE,
@@ -15,7 +15,7 @@ CREATE TABLE organization(
 );
 
 CREATE TABLE member(
-    member_id INT(4),
+    member_id INT(4) NOT NULL,
     first_name VARCHAR(25) NOT NULL,
     middle_name VARCHAR(15),
     last_name VARCHAR(15) NOT NULL,
@@ -27,4 +27,19 @@ CREATE TABLE member(
 
     PRIMARY KEY(member_id)
 );
+
+CREATE TABLE fee(
+    fee_id INT(4) NOT NULL,
+    fee_amount INT(5) NOT NULL,
+    due_date DATE NOT NULL,
+    payment_status VARCHAR(6),
+    fee_semester VARCHAR(12),
+    academic_year VARCHAR(9),
+    organization_id INT(3),
+    member_id INT(3)
+
+    PRIMARY KEY(fee_id),
+    CONSTRAINT fee_organization_id_fk FOREIGN KEY(organization_id) REFERENCES organization(organization_id),
+    CONSTRAINT fee_member)id_fk FOREIGN KEY(member_id) REFERENCES member(member_id)
+)
 
