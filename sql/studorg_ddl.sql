0e2ff5b76cf6b2405d22d9c50c80ae001f317338
Geoffrey Gabriel F. Lapaña <gflapana@up.edu.ph>
2025-05-01 14:18:28 +0800

    fix: add primary key to organization

--- sql/studorg_ddl.sql
+++ sql/studorg_ddl.sql
@@ -3,11 +3,15 @@ create database studorg;
 
 --Create Organization Tabble
 CREATE TABLE organization{
-    organization_no INT(3),
+    organization_num INT(3),
     organization_name VARCHAR(50),
     organization_type VARCHAR(20),
     date_established DATE,
     years_active INT(3),
     organization_username VARCHAR(20),
-    organization_password VARCHAR(20)
+    organization_password VARCHAR(20),
+
+    PRIMARY KEY(organization_num)
 }
+
+CREATE
