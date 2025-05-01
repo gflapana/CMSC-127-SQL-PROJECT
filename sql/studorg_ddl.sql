c8d607fd973c22a0e0cbf28102a173e9d1a17846
Geoffrey Gabriel F. Lapaña <gflapana@up.edu.ph>
2025-05-01 14:21:39 +0800

    refactor: rename organization_num to organization_id

--- sql/studorg_ddl.sql
+++ sql/studorg_ddl.sql
@@ -3,7 +3,7 @@ create database studorg;
 
 --Create Organization Tabble
 CREATE TABLE organization{
-    organization_num INT(3),
+    organization_id INT(3),
     organization_name VARCHAR(50),
     organization_type VARCHAR(20),
     date_established DATE,
@@ -11,6 +11,6 @@ CREATE TABLE organization{
     organization_username VARCHAR(20),
     organization_password VARCHAR(20),
 
-    PRIMARY KEY(organization_num)
+    PRIMARY KEY(organization_id)
 }
 
