ecfc05799b4f52faf6f007aa387aca689efc7e8e
loopsidoop <lawmacj16@gmail.com>
2025-05-26 21:19:58 +0800

    feat: add sign up (not high prio)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,8 @@
 // import './App.css'
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import LogIn from "./pages/login/login";
+import MemberSignUp from "./pages/signup/member_signup";
+import OrgSignUp from "./pages/signup/org_signup";
 
 function App() {
   const routes = [
@@ -12,9 +14,17 @@ function App() {
       element: <h1>Welcome to the CMSC 127 SQL Project</h1>
     },
     {
-      path: "/login",
+      path: "/log-in",
       element: <LogIn />
     },
+    {
+      path: "/member-sign-up",
+      element: <MemberSignUp />
+    },
+    {
+      path: "/org-sign-up",
+      element: <OrgSignUp />
+    }
   ]
   const router = createBrowserRouter(routes)
   return (
--- frontend/src/pages/login/login.jsx
+++ frontend/src/pages/login/login.jsx
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
 import { Eye, EyeOff as EyeClosed } from "lucide-react";
-
+import { useNavigate } from "react-router-dom";
 const LogIn = () => {
     const [password, setPassword] = useState("");
     const [showPassword, setShowPassword] = useState(false);
     const [toggle, setToggle] = useState(false);
+    const navigate = useNavigate();
 
     return (
         <div className={`h-screen w-screen flex items-center justify-center ${toggle ? "bg-blue-900" : "bg-gray-100"}`}>
@@ -13,7 +14,7 @@ const LogIn = () => {
                     {toggle ? "Organization" : "Member"}
                 </span>
                 <button
-                    className={`absolute top-4 right-4 w-12 h-6 flex items-center rounded-full p-1 transition-colors duration-300 ${toggle ? 'bg-white' : 'bg-gray-200'}`}
+                    className={`absolute top-4 right-4 w-12 h-6 flex items-center rounded-full p-1 transition-colors duration-300 hover:cursor-pointer ${toggle ? 'bg-white' : 'bg-gray-200'}`}
                     onClick={() => setToggle((prev) => !prev)}
                     type="button"
                 >
@@ -21,14 +22,15 @@ const LogIn = () => {
                         className={`bg-blue-500 w-4 h-4 rounded-full shadow-md transform transition-transform duration-300 ${toggle ? 'translate-x-6' : ''}`}
                     />
                 </button>
-                <h2 className="text-2xl font-bold my-6 text-center">
+                <h2 className={`text-2xl font-bold my-6 text-center ${toggle ? "text-white" : "text-blue-500"}`}>
                     Welcome Back!
                 </h2>
                 <form className="flex flex-col gap-4">
                     <input
                         type="text"
-                        placeholder={toggle? "Organization username" : "Username"}
+                        placeholder={toggle ? "Organization Username" : "Username"}
                         className={`border rounded px-3 py-2 focus:outline-none focus:ring-2 w-full ${toggle ? "focus:ring-white bg-blue-400 text-white placeholder-white border-blue-300" : "focus:ring-blue-400 text-black"}`}
+                        required
                     />
                     <div className="relative">
                         <input
@@ -37,6 +39,7 @@ const LogIn = () => {
                             value={password}
                             onChange={(e) => setPassword(e.target.value)}
                             className={`border rounded px-3 py-2 focus:outline-none focus:ring-2 w-full pr-10 ${toggle ? "focus:ring-white bg-blue-400 text-white placeholder-white border-blue-300" : "focus:ring-blue-400 text-black"}`}
+                            required
                         />
                         <button
                             type="button"
@@ -54,8 +57,18 @@ const LogIn = () => {
                         Log In
                     </button>
                     <p className={`text-sm text-center ${toggle ? "text-white" : "text-gray-600"}`}>
-                        {toggle? "Create an Organization? ": "Don't havae an account yet? "} 
-                        <a href="/sign-up" className={`${toggle ? "text-black" : "text-blue-500"} underline`} >Sign Up</a>
+                        {toggle ? "Create an Organization? " : "Don't have an account yet? "}
+                        <button
+                            onClick={() => {
+                                toggle 
+                                ? navigate('/org-sign-up', { replace: true }) 
+                                : navigate('/member-sign-up', { replace: true });
+                            }}
+                            className={`${toggle ? "text-white" : "text-blue-500"} underline hover: cursor-pointer`}
+                        >
+                            Sign Up
+                        </button>
+                        {/* <a href="/sign-up" className={`${toggle ? "text-black" : "text-blue-500"} underline`} >Sign Up</a> */}
                     </p>
                 </form>
             </div>
