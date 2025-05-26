f9b189130cae1929598dd5b8f7d9d48ad16f0c9b
Geoffrey Gabriel F. Lapaña <gflapana@up.edu.ph>
2025-05-26 18:37:54 +0800

    fix: fix order and system report 1

--- backend/organization/organization.controller.ts
+++ backend/organization/organization.controller.ts
@@ -1,13 +1,15 @@
 import pool from "../config/connectDB"
 import express from "express";
 
+
+// Queries: id (required), sex, degree_program, committee, role (committee_role), academic_year, order (order by), desc (true if desc)
 const getMembers = async (
     req: express.Request,
     res: express.Response,
     next: express.NextFunction
 ) => {
     try {
-        let query = "SELECT first_name, IFNULL(middle_name,'') middle_name, last_name, sex";
+        let query = "SELECT first_name, IFNULL(middle_name,'') middle_name, last_name, sex, degree_program, committee, committee_role, academic_year from member natural join organization_has_member";
         const conditions: string[] = ['organization_id = ?'];
         const params: (string | number)[] = [];
         let order: string | null = null;
@@ -17,38 +19,35 @@ const getMembers = async (
             params.push(req.query.id);
         }
 
-        if (req.query.role && typeof req.query.role == 'string') {
-            query += ", role";
-            conditions.push('role = ?');
-            params.push(req.query.role);
+        if (req.query.sex && typeof req.query.sex == 'string'){
+            conditions.push('sex = ?');
+            params.push(req.query.sex)
         }
 
         if (req.query.degree_program && typeof req.query.degree_program == 'string') {
-            query += ", degree_program";
             conditions.push('degree_program = ?');
             params.push(req.query.degree_program);
         }
         if (req.query.committee && typeof req.query.committee == 'string') {
-            query += ", committee";
             conditions.push('committee = ?');
             params.push(req.query.committee);
         }
-        if (req.query.committee_role && typeof req.query.committee_role == 'string') {
-            query += ", committee_role";
+        if (req.query.role && typeof req.query.role == 'string') {
             conditions.push('committee_role = ?');
-            params.push(req.query.committee_role);
+            params.push(req.query.role);
         }
         if (req.query.academic_year && typeof req.query.academic_year == 'string') {
             group = " GROUP BY member_id HAVING min(academic_year) = ?";
-            query += ", academic_year";
             params.push(req.query.academic_year);
         }
         if (req.query.order && typeof req.query.order == 'string') {
-            order = " ORDER BY ?";
-            params.push(req.query.order)
+            order = ` ORDER BY ${req.query.order}`;
+        }
+        if (req.query.desc){
+            order += " DESC";
         }
 
-        query += " from member natural join organization_has_member WHERE " + conditions.join(' AND ');
+        query += " WHERE " + conditions.join(' AND ');
 
         if (group) {
             query += group;
@@ -58,11 +57,12 @@ const getMembers = async (
             query += order;
         }
 
+        console.log(query);
         console.log(params);
-
         const conn = await pool.getConnection();
         try {
             const members = await conn.query(query, params);
+            // console.log(members);
             res.json({ members });
         } catch (err) {
             console.error(err);
@@ -74,6 +74,8 @@ const getMembers = async (
     }
 };
 
+
+// queries: id (required), academic_year (required), semester(required), order, desc
 const getUnpaidMembers = async (
     req: express.Request,
     res: express.Response,
@@ -100,8 +102,7 @@ const getUnpaidMembers = async (
         }
 
         if (req.query.order && typeof req.query.order == 'string') {
-            order = " ORDER BY ?";
-            params.push(req.query.order);
+            order = ` ORDER BY ${req.query.order}`;
         }
 
         query += " WHERE payment_status = 'Unpaid' AND " + conditions.join(' AND ');
@@ -110,8 +111,6 @@ const getUnpaidMembers = async (
         if (order) {
             query += order;
         }
-
-        console.log(params);
 
         const conn = await pool.getConnection();
         try {
