548fae80db24349912beab8adc3ca58e8f20af35
Geoffrey Gabriel F. Lapaña <gflapana@up.edu.ph>
2025-05-28 00:34:25 +0800

    feat: add editDetails to member

--- backend/index.ts
+++ backend/index.ts
@@ -3,6 +3,7 @@ import express from "express";
 import cors from "cors";
 import organizationRouter from "./organization/organization.route";
 import authRouter from "./auth/auth.route";
+import memberRouter from "./member/member.route";
 
 const app: express.Express = express();
 
@@ -12,6 +13,7 @@ app.use(express.urlencoded({ extended: true })); // for form submissions
 
 app.use("/api/auth", authRouter);
 app.use("/api/organization", organizationRouter);
+app.use("/api/member", memberRouter);
 
 
 const PORT = process.env.PORT;
